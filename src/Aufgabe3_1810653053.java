84d45d263bbe4ebac05d7851bc92094585b00b52
Your Name <you@example.com>
2018-11-27 09:46:37 +0100

    Aufgabe4_2

--- src/Aufgabe3_1810653053.java
+++ src/Aufgabe3_1810653053.java
@@ -1,2 +1,14 @@
-public class Aufgabe3_1810653053 {
+import javax.swing.*;
+
+public class Aufgabe3_1810653053
+{
+    public static void main (String[]args)
+    {
+        double alter;
+        String eingabe;
+        eingabe= JOptionPane.showInputDialog("bitte geben sie euren Alter ein");
+        alter=Double.parseDouble(eingabe);
+        JOptionPane.showMessageDialog(null, "euer Alter ist:"+(String.valueOf(alter)));
+
+    }
 }
--- src/Aufgabe4_1810653053.java
+++ src/Aufgabe4_1810653053.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Aufgabe4_1810653053 {
+    public static void main (String[]args)
+    {
+        int w=Integer.parseInt(JOptionPane.showInputDialog("bitte geben sie Zahl zw. 1 und 3 ein, um sich durch die Schleifen zu orientieren"));
+        int i = 0;
+        if (w==1)
+        {
+            while (10 > i)
+            {
+                System.out.println("Der Schleifenzähler ist:" + (++i));
+            }
+        }
+        if (w==2)
+        {
+            do
+                {
+                System.out.println("Der Schleifenzähler ist:" + (++i));
+            } while (10 > i);
+
+        }
+        if (w==3)
+        {
+            for (i=1;11 > i;i++)
+            System.out.println("Der Schleifenzähler ist:" + (i));
+        }
+
+    }
 }
